7b909933fa3926f947ce679f86604ba9d215003a
xelarr <35121498+xelarr@users.noreply.github.com>
2020-03-11 23:34:44 +0000

    Add files via upload

--- PortMonitor.py
+++ PortMonitor.py
@@ -2,9 +2,10 @@ import socket
 import psutil
 import threading
 import time
+import ProcessFinder
 from queue import Queue
 import select
-PortArray = [[None for _ in range(1)] for _ in range(1)]
+PortsArray = [[None for _ in range(1)] for _ in range(1)]
 def get_ip():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     try:
@@ -47,13 +48,13 @@ def PortScanner(Port, var1):
 
     if TCPResult1 == 0 or TCPResult2 == 0:
         with print_lock:
-            PortArray.append([Port, "TCP"])
+            PortsArray.append([Port, "TCP"])
             var1 += 1
             print(Port, " is open and TCP")
             
     elif data != '':
         with print_lock:
-            PortArray.append([Port, "UDP"])
+            PortsArray.append([Port, "UDP"])
             var1 += 1
             print(Port, "is open and UDP")
     """else:
@@ -62,65 +63,53 @@ def PortScanner(Port, var1):
     "Port += 1"
     TCPSocket.close()
     UDPSocket.shutdown(socket.SHUT_RDWR)
-    return PortArray
-"""
-for proc in psutil.process_iter():
-    try:
-        # Get process name & pid from process object.
-        processName = proc.name()
-        processID = proc.pid
-        print(processName , ' ::: ', processID)
-    except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
-        pass
-"""
+    return PortsArray
+
+def FindProcess(PortsArray):
+    for i in range(len(PortsArray)):
+        try:
+            for proc in psutil.process_iter():
+                for conns in proc.connections(kind='inet'):
+                    if conns.laddr.port == PortsArray[i][0]:
+                        PortsArray[i].append(proc.name())
+                        PortsArray[i].append(proc.io_counters().other_bytes)
+                        raise Exception()
+        except Exception:
+            continue
+    return PortsArray
 
 
 
+def GetPorts():
 
-def threader():
-    while True:
-        # gets an worker from the queue
-        worker = q.get()
+    def threader():
+        while True:
+            # gets an worker from the queue
+            worker = q.get()
 
-        # Run the example job with the avail worker in queue (thread)
-        PortArray = PortScanner(worker, var1)
+            # Run the example job with the avail worker in queue (thread)
+            PortsArray = PortScanner(worker, var1)
+        
+            # completed with the job
+            q.task_done()
 
-        # completed with the job
-        q.task_done()
+    q = Queue()
+    for x in range(5000):
+         t = threading.Thread(target=threader)
 
+         # classifying as a daemon, so they will die when the main dies
+         t.daemon = True
 
+         # begins, must come after daemon definition
+         t.start()
 
-q = Queue()
+    start = time.time()
 
-for x in range(5000):
-     t = threading.Thread(target=threader)
+    for worker in range(1,65535):
+        q.put(worker)
 
-     # classifying as a daemon, so they will die when the main dies
-     t.daemon = True
+    q.join()
 
-     # begins, must come after daemon definition
-     t.start()
-
-
-start = time.time()
-
-for worker in range(1,65535):
-    q.put(worker)
-
-
-
-
-q.join()
-
-for i in range(len(PortArray)):
-    try:
-        for proc in psutil.process_iter():
-            for conns in proc.connections(kind='inet'):
-                if conns.laddr.port == PortArray[i][0]:
-                    PortArray[i].append(proc.name())
-                    PortArray[i].append(proc.io_counters().other_bytes)
-                    raise Exception()
-    except Exception:
-        continue
-
-print('\n'.join(['\t'.join([str(cell) for cell in row]) for row in PortArray]))
+    PortArray = FindProcess(PortsArray)
+    "PortArray.sort()"
+    return PortsArray
--- UI.py
+++ UI.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkintertable import *
 import PacketSniffer
+import PortMonitor
 
 
 class Page(Frame):
@@ -13,68 +14,106 @@ class Home(Page):
     def __init__(self, master=None):
         Page.__init__(self, master)
         AlertsPage = Alerts(self)
-        PortTable = Treeview(self)
-        PortTable['columns'] = ('Application', 'Protocol', 'Sending(B/sec)', 'Recieving(B/sec)', 'View')
-        PortTable.heading("#0", text='Port No.', anchor='w')
-        PortTable.column("#0", anchor="w", width=30)
-        PortTable.heading('Application', text='Application')
-        PortTable.column('Application', anchor='center', width=100)
-        PortTable.heading('Protocol', text='Protocol')
-        PortTable.column('Protocol', anchor='center', width=100)
-        PortTable.heading('Sending(B/sec)', text='Sending(B/sec)')
-        PortTable.column('Sending(B/sec)', anchor='center', width=100)
-        PortTable.heading('Recieving(B/sec)', text='Recieving(B/sec)')
-        PortTable.column('Recieving(B/sec)', anchor='center', width=100)
-        PortTable.heading('View', text='View')
-        PortTable.column('View', anchor='center', width=30)
-        PortTable.grid(sticky = (N, W, E))
-        self.treeview = PortTable
-        Label(self, text='Number of Alerts: ', borderwidth=1).grid(sticky=(S, W), padx=100, pady=0)
-        Label(self, text='Highest Priority: ', borderwidth=1).grid(sticky=(S, W), padx=100, pady=50)
-        Button(self, text='Go To Alerts').grid(sticky=(S, W), padx=200, pady=30)
+        PortPage = Port(self)
+        self.PortTable = Treeview(self, selectmode='browse')
+        self.PortTable.pack()
+        self.PortTable.place(x = 0, y = 20, height = 500, width = 1190)
+
+        scrollbary = Scrollbar(self, orient='vertical')
+        scrollbary.config(command=self.PortTable.yview)
+        scrollbary.pack(side=RIGHT, fill=Y)
+        
+        self.PortTable['columns'] = ('Port No.', 'Application', 'Protocol', 'Sending(B/sec)', 'Recieving(B/sec)', 'View')
+        self.PortTable['show'] = 'headings'
+        self.PortTable.heading("Port No.", text='Port No.')
+        self.PortTable.column("Port No.", anchor="center", width=30)
+        self.PortTable.heading('Application', text='Application')
+        self.PortTable.column('Application', anchor='center', width=100)
+        self.PortTable.heading('Protocol', text='Protocol')
+        self.PortTable.column('Protocol', anchor='center', width=100)
+        self.PortTable.heading('Sending(B/sec)', text='Sending(B/sec)')
+        self.PortTable.column('Sending(B/sec)', anchor='center', width=100)
+        self.PortTable.heading('Recieving(B/sec)', text='Recieving(B/sec)')
+        self.PortTable.column('Recieving(B/sec)', anchor='center', width=100)
+        self.PortTable.heading('View', text='View')
+        self.PortTable.column('View', anchor='center', width=30)
+       
+        
+        Label(self, text='Number of Alerts: ', borderwidth=1).place(x=50, y=600)
+        Label(self, text='Highest Priority: ', borderwidth=1).place(x=50, y=650)
+        Button(self, text='Go To Alerts', command=master.LiftAlerts).place(x=100, y=700)
         self.grid_rowconfigure(0, weight = 1)
         self.grid_columnconfigure(0, weight = 1)
         
         
-
+        PortsArray = PortMonitor.GetPorts()
+        index = iid = 0
         
+        for row in PortsArray:
+            self.PortTable.insert("", index, iid, values=row)
+            index = iid = index + 1
+
+        self.AnswerBox = Entry(self)
+        self.AnswerBox.place(x=550, y=550)
+        Button(self, command=master.LiftPort, text="View Port").place(x=600, y=547)
         
 class Port(Page):
     def __init__(self, master=None):
         Page.__init__(self, master)
 
 class Capture(Page):
+    
     def __init__(self, master=None):
         Page.__init__(self, master)
-        CaptureTable = Treeview(self)
-        CaptureTable['columns'] = ['Source', 'Destination', 'Protocol', 'Information']
-        CaptureTable['show'] = 'headings'
-        CaptureTable.heading('Source', text='Source')
-        CaptureTable.column('Source', anchor="w", width=100)
-        CaptureTable.heading('Destination', text='Destination')
-        CaptureTable.column('Destination', anchor='center', width=100)
-        CaptureTable.heading('Protocol', text='Protocol')
-        CaptureTable.column('Protocol', anchor='center', width=100)
-        CaptureTable.heading('Information', text='Information')
-        CaptureTable.column('Information', anchor='e', width=100)
-        CaptureTable.grid(sticky = (N,E,S,W))
-        self.treeview = CaptureTable
+        self.CaptureTable = Treeview(self)
+        self.CaptureTable.pack()
+        self.CaptureTable.place(x = 0, y = 0, height = 650, width = 1190)
+
+        scrollbary = Scrollbar(self, orient='vertical')
+        scrollbary.config(command=self.CaptureTable.yview)
+        scrollbary.pack(side=RIGHT, fill=Y)
+        self.CaptureTable['columns'] = ['Source', 'Destination', 'Protocol', 'Information']
+        self.CaptureTable['show'] = 'headings'
+        self.CaptureTable.heading('Source', text='Source')
+        self.CaptureTable.column('Source', anchor="w", width=100)
+        self.CaptureTable.heading('Destination', text='Destination')
+        self.CaptureTable.column('Destination', anchor='center', width=100)
+        self.CaptureTable.heading('Protocol', text='Protocol')
+        self.CaptureTable.column('Protocol', anchor='center', width=100)
+        self.CaptureTable.heading('Information', text='Information')
+        self.CaptureTable.column('Information', anchor='e', width=100)
+        self.treeview = self.CaptureTable
+        self.AnswerBox = Entry(self)
+        self.AnswerBox.place(x=550, y=700)
+        CaptureArray = Button(self, command=self.CapturePackets, text="Capture Packets").place(x=700, y=698)
+        Label(self, text='Enter the number of packets to sniff', borderwidth=1).place(x=540, y=670)
+
         
-        self.grid_rowconfigure(0, weight = 1)
-        self.grid_columnconfigure(0, weight = 1)
-
-
-        Array = PacketSniffer.CapturePackets(100)
+        
+    def CapturePackets(self):
+        PacketsNumberString = self.AnswerBox.get()
+        PacketsNumberInt = int(PacketsNumberString)
+        CaptureArray = PacketSniffer.CapturePackets(PacketsNumberInt)
+        self.UpdateTable(CaptureArray)
+    def UpdateTable(self, CaptureArray):
+        for i in self.CaptureTable.get_children():
+            self.CaptureTable.delete(i)
         index = iid = 0
-        for row in Array:
-            CaptureTable.insert("", index, iid, values=row)
+        for row in CaptureArray:
+            self.CaptureTable.insert("",index, iid, values=row)
             index = iid = index + 1
-        
         
 class Alerts(Page):
     def __init__(self, master=None):
         Page.__init__(self, master)
         AlertTable = Treeview(self)
+        AlertTable.pack()
+        AlertTable.place(x = 0, y = 0, height = 700, width = 1190)
+
+        scrollbary = Scrollbar(self, orient='vertical')
+        scrollbary.config(command=AlertTable.yview)
+        scrollbary.pack(side=RIGHT, fill=Y)
+        
         AlertTable['columns'] = ('Status', 'Info', 'Priority', 'Solved')
         AlertTable.heading("#0", text='Port', anchor='w')
         AlertTable.column("#0", anchor="w", width=100)
@@ -86,10 +125,7 @@ class Alerts(Page):
         AlertTable.column('Priority', anchor='center', width=100)
         AlertTable.heading('Solved', text='Solved')
         AlertTable.column('Solved', anchor='center', width=100)
-        AlertTable.grid(sticky = (N,E,S,W))
         self.treeview = AlertTable
-        self.grid_rowconfigure(0, weight = 1)
-        self.grid_columnconfigure(0, weight = 1)
 
 class Window(Frame):
     def __init__(self, master=None):
@@ -97,9 +133,9 @@ class Window(Frame):
         self.master = master
         self.pack(fill=BOTH, expand=1)
         HomePage = Home(self)
-        PortPage = Port(self)
+        self.PortPage = Port(self)
         CapturePage = Capture(self)
-        AlertsPage = Alerts(self)
+        self.AlertsPage = Alerts(self)
 
         buttonframe = Frame(self)
         container = Frame(self)
@@ -107,16 +143,20 @@ class Window(Frame):
         container.pack(side="top", fill="both", expand=True)
         
         HomePage.place(in_=container, x=0, y=25, relwidth=1, relheight=1)
-        PortPage.place(in_=container, x=0, y=25, relwidth=1, relheight=1)
+        self.PortPage.place(in_=container, x=0, y=25, relwidth=1, relheight=1)
         CapturePage.place(in_=container, x=0, y=25, relwidth=1, relheight=1)
-        AlertsPage.place(in_=container, x=0, y=25, relwidth=1, relheight=1)
+        self.AlertsPage.place(in_=container, x=0, y=25, relwidth=1, relheight=1)
         HomeButton = Button(self, text="Home", command=HomePage.lift)
-        HomeButton.place(x=0, y=0)
+        HomeButton.place(x=0, y=0, width=400)
         CaptureButton = Button(self, text="Capture", command=CapturePage.lift)
-        CaptureButton.place(x=74, y=0)
-        AlertsButton = Button(self, text="Alerts", command=AlertsPage.lift)
-        AlertsButton.place(x=147, y=0)
-        
+        CaptureButton.place(x=400, y=0, width = 400)
+        AlertsButton = Button(self, text="Alerts", command=self.AlertsPage.lift)
+        AlertsButton.place(x=800, y=0, width = 400)
+    def LiftAlerts(self):
+        self.AlertsPage.lift()
+    
+    def LiftPort(self):
+        self.PortPage.lift()
     
         
 
@@ -126,6 +166,7 @@ app = Window(root)
 
 # set window title
 root.wm_title("Port Monitor")
-root.wm_geometry("800x400")
+root.wm_geometry("1200x800")
+root.resizable(width=False, height=False)
 # show window
 root.mainloop()
