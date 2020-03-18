3f3653e3497a9850af3bceeaaa1cd235c6fedd4c
xelarr <35121498+xelarr@users.noreply.github.com>
2020-03-18 19:32:23 +0000

    Displays suspicious ttl values

--- PacketSniffer.py
+++ PacketSniffer.py
@@ -1,19 +1,28 @@
 from scapy.all import *
 import socket
 
-PacketsArray = [[None for _ in range(1)] for _ in range(1)]
-import socket
 
 ProtocolLookupTable = {num:name[8:] for name,num in vars(socket).items() if name.startswith("IPPROTO")}
 
 def CapturePackets(NumOfPackets):
-    for i in range(0, NumOfPackets):
-        sniff(filter='ip', prn=StorePacket, count=1)
-    return PacketsArray
-
-def StorePacket(pkt):
-        PacketsArray.append([pkt[IP].src, pkt[IP].dst, ProtocolLookupTable[pkt.proto]])
+    PacketsArray = [[None for _ in range(1)] for _ in range(1)]
+    def GetPackets(NumOfPackets):
+        for i in range(0, NumOfPackets):
+            sniff(filter='ip', prn=StorePacket, count=1)
         return PacketsArray
+
+    def StorePacket(pkt):
+        try:
+            if (1 < pkt[IP].ttl <= 30) or (64 < pkt[IP].ttl <= 98) or (128 < pkt[IP].ttl <= 225):
+                PacketsArray.append([pkt[IP].src, pkt[IP].dst, ProtocolLookupTable[pkt.proto],  pkt[IP].ttl])
+            else:
+                PacketsArray.append([pkt[IP].src, pkt[IP].dst, ProtocolLookupTable[pkt.proto]])
+        except KeyError:
+            PacketsArray.append([pkt[IP].src, pkt[IP].dst, "IGMP"])
+        return PacketsArray
+    PacketsArray = GetPackets(NumOfPackets)
+    PacketsArray.pop(0)
+    return PacketsArray
     
 
 
